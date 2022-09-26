a2f88e6a266b5eff4c22447ecafa652183dcaa85
lpb <462570646@qq.com>
2022-09-26 14:29:25 +0800

    add repo

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -13,7 +13,8 @@ const config = defineUserConfig({
     ],
     lastUpdatedText: '上次更新时间',
     colorMode:'light',
-    colorModeSwitch:true
+    colorModeSwitch:true,
+    repo:'https://github.com/Lpbzzz/vuePress.git'
   }),
 });
 
