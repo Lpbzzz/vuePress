548fbceef89cbb7dba70ef398d544651243091b4
lpb <462570646@qq.com>
2022-09-26 14:00:42 +0800

    init

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,21 @@
+import { defaultTheme, defineUserConfig } from "vuepress";
+
+const config = defineUserConfig({
+  lang: 'zh-CN',
+  title: 'VuePress',
+  description: 'Vue 驱动的静态网站生成器',
+  theme: defaultTheme({
+    navbar: [
+      {
+        text: '首页',
+        link: '/',
+      },
+    ],
+    lastUpdatedText: '上次更新时间',
+    colorMode:'light',
+    colorModeSwitch:true
+  }),
+});
+
+
+export default config;
